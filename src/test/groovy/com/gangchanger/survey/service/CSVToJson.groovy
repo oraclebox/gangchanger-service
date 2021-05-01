fe0891cac6d608de38035e361f6951d8ad15f454
ROGER CHAN <oraclebox@gmail.com>
2021-05-01 16:01:06 +0800

    Comment unused code.

--- src/test/groovy/com/gangchanger/survey/service/CSVToJson.groovy
+++ src/test/groovy/com/gangchanger/survey/service/CSVToJson.groovy
@@ -11,18 +11,17 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class CSVToJson {
 
-	@Ignore
 	@Test
 	void convert() {
-		String csv = new File('testing/category.csv').text;
-		String[] line = StringUtils.splitByWholeSeparatorPreserveAllTokens(csv, "\n");
-		List<Software> softwares = [];
-		for(int i = 1; i < line.length - 1; i++){
-			String[] fields = StringUtils.splitByWholeSeparatorPreserveAllTokens(line[i], ",");
-			if(fields.size() >= 6) {
-				softwares.add(new Software(category: fields[2], imgSrc: fields[4], name: fields[5], rate: Double.parseDouble(fields[6])));
-			}
-		}
+//		String csv = new File('testing/category.csv').text;
+//		String[] line = StringUtils.splitByWholeSeparatorPreserveAllTokens(csv, "\n");
+//		List<Software> softwares = [];
+//		for(int i = 1; i < line.length - 1; i++){
+//			String[] fields = StringUtils.splitByWholeSeparatorPreserveAllTokens(line[i], ",");
+//			if(fields.size() >= 6) {
+//				softwares.add(new Software(category: fields[2], imgSrc: fields[4], name: fields[5], rate: Double.parseDouble(fields[6])));
+//			}
+//		}
 
 	}
 
