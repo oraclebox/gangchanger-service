75648a98a2bb74bab10e59e51e53c5ef0903de93
ROGER CHAN <oraclebox@gmail.com>
2021-05-19 15:48:13 +0800

    Add suggestion.

--- src/main/groovy/com/gangchanger/survey/service/dto/Survey.groovy
+++ src/main/groovy/com/gangchanger/survey/service/dto/Survey.groovy
@@ -1,5 +1,6 @@
 package com.gangchanger.survey.service.dto
 
+import jdk.nashorn.internal.ir.annotations.Ignore
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
@@ -8,14 +9,22 @@ import org.springframework.data.mongodb.core.mapping.Document
 class Survey {
     @Id
     String id;
+    @Ignore
     @Indexed
     String email;
     @Indexed
     String hash;
 
     List<Choice> choices = [];
+    List<Suggestion> suggestions = [];
     Date updateTs = new Date();
+    @Ignore
     boolean sentWelcomeMail;
+
+    static class Suggestion{
+        String name;
+        String platform;
+    }
 
     static class Choice{
         Software software;
